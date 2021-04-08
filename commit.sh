da6ea81cee3d0e71dc69dbbfbc9cb85e8eb404dd
StandleyWu <>
2021-04-08 20:06:44 +0000

    third commit

--- commit.sh
+++ commit.sh
@@ -3,5 +3,5 @@ git add --all
 git config --global user.name "standey6"
 git config --global user.email "standleywu2006@gmail.com"
 git commit -m "second commit"
-git remote add origin https://github.com/standley6/standley6.github.git
+git remote add origin https://github.com/standley6/standley6.github.io.git
 git push -u origin master
---
+++ commit2.sh
@@ -0,0 +1,7 @@
+git init
+git add --all
+git config --global user.name "standley6"
+git config --global user.email "standleywu2006@gmail.com"
+git commit -m "third commit"
+git remote add origin linktogithub
+git push -u origin master
