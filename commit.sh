86daaa6837b5dea764c865f49473ce8921c9705e
StandleyWu <>
2021-04-08 19:46:35 +0000

    second commit

---
+++ commit.sh
@@ -0,0 +1,7 @@
+git init
+git add --all
+git config --global user.name "standey6"
+git config --global user.email "standleywu2006@gmail.com"
+git commit -m "second commit"
+git remote add origin https://github.com/standley6/standley6.github.git
+git push -u origin master
